2f721317738c65b41ca34d8a4589306978b0da29
zep1994 <snaxbags@protonmail.com>
2018-12-21 21:17:35 -0600

    Add rails api backend

--- app/controllers/v1/games_controller.rb
+++ app/controllers/v1/games_controller.rb
@@ -4,5 +4,42 @@ class V1::GamesController < ApplicationController
         render json: @games, status: :ok
     end 
 
-    
+    def show
+        render json: @game, status: :ok
+    end
+
+    def create 
+        @game = Game.new(game_params)
+
+        if @game.save
+            render json: @game, status: :created
+        else 
+            render json: { errors: @game.errors.full_messages,
+            message: 'Can not create game because:' }, status: 422
+       end
+    end
+
+    def update
+        @game.update(game_params)
+        render json: @game, status: :ok
+    end
+
+    def destroy 
+        @game.destroy
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(
+            :id,
+            :name,
+            directions_attributes: [:id, :rules],
+            drinks_attributes: [:id, :name]
+        )
+    end
+
+    def set_game
+        @game = Game.find(params[:id])
+    end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,7 @@
 class Game < ApplicationRecord
     has_many :directions
     has_many :drinks
+
+    accepts_nested_attributes_for :drinks, :allow_destroy => true
+    accepts_nested_attributes_for :directions, :allow_destroy => true
 end
