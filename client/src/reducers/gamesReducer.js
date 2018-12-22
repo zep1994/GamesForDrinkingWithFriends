26793684b51f53e808c6ce1ddcd288aa6a734e11
zep1994 <snaxbags@protonmail.com>
2018-12-21 22:04:23 -0600

    Update Reducers

--- client/src/reducers/gamesReducer.js
+++ client/src/reducers/gamesReducer.js
@@ -4,6 +4,15 @@ export default (state = [], action) => {
             return state.concat(action.game)
         }
 
+        case 'REMOVE_GAME': {
+            const idx = state.findIndex(game => game.id === action.gameId);
+            return [...state.slice(0, idx), ...state.slice(idx + 1)]
+          }
+      
+          case 'SET_GAME': {
+            return action.games;
+          }
+
     default: {
         return state
       }
