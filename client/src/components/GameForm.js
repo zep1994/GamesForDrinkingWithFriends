413bf1277042a72ccb8bfc8b1ee365027b39e72e
zep1994 <snaxbags@protonmail.com>
2018-12-22 20:48:48 -0600

    Add Name to game form

--- client/src/components/GameForm.js
+++ client/src/components/GameForm.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class GameForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: '',
+      directions_attributes: [{ rules: ''}],
+      drinks_attributes: [{ name: '' }]
+    }
+  }
+
+
   render() {
     return(
       <div>
-        <h1>Game Form</h1>
+        <h1>Add Game: </h1>
+        <hr  className="divider"/>
+        <form> 
+          <div>
+            <label>Game Name: </label>
+              <input
+              type="text"
+              value={this.state.name}
+              onChange={this.handleName}
+              />
+          </div>
+        </form>
       </div>
     );
   }
